e29238f2889d26186d853e5f6f17e69b1a9bbc3f
Nithon <56203456+NithonF@users.noreply.github.com>
2020-11-10 09:52:28 +0300

    Add files via upload

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import sqlite3
 
 from PyQt5 import uic, QtCore
 from PyQt5.QtWidgets import *
@@ -10,11 +11,17 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('проект.ui', self)
 
-        self.count_misses = 0
-        self.count_hits = 0
+        self.start_btn.hide()
+        self.name.hide()
+        self.max_points.hide()
+        self.enter_window.hide()
+        self.registration_window.hide()
 
-        self.name_player = str(self.input_name.text())
-        self.add_datebase()
+        self.open_database()
+        self.names_players = list(map(lambda x: x[0], self.cur.execute("SELECT name FROM players_score").fetchall()))
+
+        self.entrence.clicked.connect(self.enter_window_function)
+        self.registration.clicked.connect(self.regist)
 
         self.buttons = QButtonGroup()
         for i in range(30):
@@ -28,18 +35,81 @@ class MyWidget(QMainWindow):
             self.buttons.setId(self.button, i)
 
         self.start_btn.clicked.connect(self.start_game)
-        self.f_btn.setStyleSheet('background: rgb(255, 255, 255); border: 2px solid rgb(255, 255, 255)')
-        self.f_btn.clicked.connect(self.function_misses)
 
-    def add_datebase(self):
-        pass
+    def regist(self):
+        self.registration_window.show()
+        self.registr_btn.clicked.connect(self.registration_window_function)
+        self.cancel_btn_enter_2.clicked.connect(self.cancel_registration)
+
+    def registration_window_function(self):
+        self.reg_password = str(self.input_password_reg.text())
+        self.reg_name = str(self.name_input_reg.text())
+        if self.reg_name not in self.names_players:
+            self.window_name.setText('имя свободно')
+            self.player_name = self.reg_name
+            self.add_in_datebase(self.reg_name, self.reg_password)
+            self.registration_window.hide()
+            self.show_start()
+        else:
+            self.window_name.setText('имя занято')
+
+    def show_start(self):
+        self.entrence.hide()
+        self.registration.hide()
+        self.name.show()
+        self.start_btn.show()
+        self.max_points.show()
+        self.name.setText(self.player_name)
+        score = self.cur.execute(f"""SELECT score FROM players_score
+                    WHERE name='{self.player_name}'""").fetchall()[0]
+        self.max_points.setText(str(score[0]))
+
+    def cancel_registration(self):
+        self.registration_window.hide()
+
+    def enter_window_function(self):
+        self.enter_window.show()
+        self.enter_btn.clicked.connect(self.enter)
+        self.cancel_btn_enter.clicked.connect(self.cancel_enter)
+
+    def cancel_enter(self):
+        self.enter_window.hide()
+
+    def enter(self):
+        self.enter_name = str(self.name_input.text())
+        self.enter_password = str(self.password.text())
+        if self.enter_name in self.names_players:
+            password = str(self.cur.execute(f"""SELECT password FROM players_score
+                    WHERE name='{self.enter_name}'""").fetchall()[0][0])
+            if password == self.enter_password:
+                self.player_name = self.enter_name
+                self.enter_window.hide()
+                self.show_start()
+            else:
+                self.error_w.setText('неверный пароль')
+        else:
+            self.error_w.setText('неверное имя')
+
+    def open_database(self):
+        self.con = sqlite3.connect('score.db')
+        self.cur = self.con.cursor()
+
+    def add_in_datebase(self, name, password):
+        self.cur.execute(f'''INSERT INTO players_score VALUES ('{len(self.names_players) + 1}', 
+'{name}', 0, '{password}')''')
+        self.con.commit()
 
     def start_game(self):
+        self.count_misses = 0
+        self.count_hits = 0
         self.count = 0
+        self.dif_time_1 = 0
+        self.f_btn.clicked.connect(self.function_misses)
         self.make_btns()
-        self.timer()
+        self.make_timer()
+        self.start_btn.hide()
 
-    def timer(self):
+    def make_timer(self):
         self.timer = QtCore.QTimer()
         self.time = QtCore.QTime(0, 0, 0)
 
@@ -63,15 +133,27 @@ class MyWidget(QMainWindow):
         self.buttons.buttonClicked.connect(self.function_btn)
 
     def function_btn(self, this_button):
+        self.dif_time_2 = int(self.time.toString('ss'))
+        self.kof_time = self.dif_time_2 - self.dif_time_1
+        if self.kof_time == 0:
+            self.kof_time = 1
         this_button.hide()
         self.count_hits += 1
-        self.count += 100
+        self.count += 100 * 1 / self.kof_time
         self.hits.display(self.count_hits)
         self.rezult.display(self.count)
+        self.dif_time_1 = self.dif_time_2
+        if self.count_hits == 30:
+            self.finish_game()
+
+    def finish_game(self):
+        self.timer.stop()
+        self.time = QtCore.QTime(0, 0, 0)
+        self.start_btn.show()
 
     def function_misses(self):
         self.count_misses += 1
-        self.count -= 150
+        self.count -= 50
         self.misses.display(self.count_misses)
         self.rezult.display(self.count)
 
